c5a18cf5cd10f6f36170ff40e21e66b005defff6
qjootkar1 <qjootkar@proton.me>
2026-02-13 20:21:29 +0900

    Add FastAPI and Jinja2 template setup

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request, Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
